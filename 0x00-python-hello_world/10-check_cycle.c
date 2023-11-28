c08e0700523bde3faefdbc0e56c9eb6b1f6aae3f
LegendaryTunzeverywhere <princecph4@gmail.com>
2023-11-27 17:41:19 -0800

    Added new file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * check_cycle - Entry point
+ * @list: pointer to the list
+ *
+ * Description: checks if singly linked list is a cycle or not.
+ * Returb: 0 if no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_ *list)
+{
+	listint_t *f = list;
+	listint_t *s = list;
+
+	if (!list)
+		return (0);
+
+	while (1)
+	{
+		if (f->next != NULL && f->next->next != NULL)
+		{
+			f = f->next->next;
+			s = s->next;
+
+			if (f == s)
+				return (1);
+		}
+		else
+			return (0);
+	}
+}
