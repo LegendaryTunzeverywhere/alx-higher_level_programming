6a5b8128cff45f3361a1b9fe904cf170b7b23dd9
LegendaryTunzeverywhere <princecph4@gmail.com>
2023-11-30 15:57:21 -0800

    Initial Commits

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def print_arg(argv):
+    size = len(argv) - 1
+    if size == 0:
+        print("{:d} argument.".format(size))
+        return
+    else:
+        if size == 1:
+            print("{:d} argument:".format(size))
+        else:
+            print("{:d} argument:".format(size))
+        a = 1
+        while a <= size:
+            print("{:d}: {:s}".format(a, argv[a]))
+            a += 1
+
+if __name__ == "__main__":
+    import sys
+    print_arg(sys.argv)
