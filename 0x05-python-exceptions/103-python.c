940160768fac34f044ea5fde32c9abcf88d4e0a2
Adewale Okunade Christopher <princecph4@gmail.com>
2023-12-26 17:01:55 +0000

    contd

---
+++ 0x05-python-exceptions/103-python.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * print_python_bytes - Entry point
+ * @p: python object
+ * Description: print some basic info about Python bytes objects 
+ * Return: nothing
+ */
+void print_python_bytes(PyObject *p)
+{
+}
+
+/**
+ * print_python_float - Entry point
+ * @p: python float
+ * Description: print Python float
+ * Return: nothing
+ */
+void print_python_float(PyObject *p)
+{
+}
+
+/**
+ * print_python_list - Entry point
+ * @p: python object
+ * Description: print some basic info about Python lists
+ * Return: nothing
+ */
+void print_python_list(PyObject *p)
+{
+}
