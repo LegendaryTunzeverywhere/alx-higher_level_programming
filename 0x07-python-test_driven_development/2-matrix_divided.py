fd7a033514637b947469bd55c54e3f7ecd88a4db
Adewale Okunade Christopher <princecph4@gmail.com>
2024-01-10 03:05:51 +0000

    Task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,11 +3,27 @@
 
 
 def matrix_divided(matrix, div):
-    """Method to divide the matric list.
-    Returns a new matrix
     """
-    if not all(isinstance(elements, list) for elements in matrix:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    else:
-        if not all(isinstance(elements, (int, float)) for elements in matrix:
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    divides all elemnts of a matrix
+    """
+    size = None
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
+    for l in matrix:
+        if type(l) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+        if size is None:
+            size = len(l)
+        elif size != len(l):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in l:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(i / div, 2) for i in l] for l in matrix]
