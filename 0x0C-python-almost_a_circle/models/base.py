1d4622ed8039b96ac7ebaeab91e0a36e3c8ff651
LegendaryTunz <princecph4@gmail.com>
2024-01-17 07:01:46 -0800

    Adjustments needed

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defines a model class."""
+import json
+import csv
+import turtle
+
+class Base:
+    """Base model.
+
+    This indicates the "Base" for all classes in this project.
+
+    Private Class Attributes:
+    __nb_object (init): Number of instantiated Bases.
+    """
+
+    def __init__
